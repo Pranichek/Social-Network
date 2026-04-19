7257d5fe1c71ae4070736db8da430ecfc6b4705c
Vova Yakovets <149181189+voyak2010@users.noreply.github.com>
2026-04-19 13:39:13 +0300

    Зробив сторінкцу реєстрації

--- Social_network/user_app/urls.py
+++ Social_network/user_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('settings/', SettingsView.as_view(), name = 'settings_view'),
-    path('register/', RegistrationView.as_view(), name = 'registration_view'),
-    path('registration/', CheckRegistration.as_view(), name='check_registration'),
-    path('logout/', LogoutView.as_view(), name='logout_view'),
-    path('login/', LoginView.as_view(), name='login_view'),
-    path('check_login/', CheckLogin.as_view(), name='check_login_view'),
+    path("settings/", SettingsView.as_view(), name="settings_view"),
+    path("register/", RegistrationView.as_view(), name="registration_view"),
+    path("registration/", CheckRegistration.as_view(), name="check_registration"),
+    path("logout/", LogoutView.as_view(), name="logout_view"),
+    path("login/", LoginView.as_view(), name="login_view"),
+    path("check_login/", CheckLogin.as_view(), name="check_login_view"),
 ]
