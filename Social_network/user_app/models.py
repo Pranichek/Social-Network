660d585f0f97f5ce9efc531de2fa2a1fb7e981dc
Hrinchenko Volodymyr <vovagrinchenko19@gmail.com>
2026-04-23 17:55:48 +0300

    Створили перемикач паролю

--- Social_network/user_app/models.py
+++ Social_network/user_app/models.py
@@ -1,6 +1,20 @@
-# from django.db import models
+from django.db import models
 
 # # Create your models here.
 # class User(models.Model):
 #     name = models.CharField(max_lenght = 100)
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     
