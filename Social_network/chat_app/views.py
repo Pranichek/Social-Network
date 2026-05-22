e18c719922d473900a895ac81517c3557542738d
MaksymS-rgb <maksymselifanov055@gmail.com>
2026-05-22 13:17:06 +0300

    chats css

--- Social_network/chat_app/views.py
+++ Social_network/chat_app/views.py
@@ -5,3 +5,10 @@ from django.views.generic import TemplateView
 # Create your views here.
 class ChatView(LoginRequiredMixin, TemplateView):
     template_name = 'chat_app/chats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['private_messages'] = 'private_messages'
+        context['group_messages'] = 'group_messages'
+        
+        return context
