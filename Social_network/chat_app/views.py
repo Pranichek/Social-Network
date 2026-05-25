bf4d74fa7fdc8853a15b5f37a5c7a82d90f11134
Hrinchenko Volodymyr <vovagrinchenko19@gmail.com>
2026-05-25 16:13:45 +0300

    Створили форми та моделі

--- Social_network/chat_app/views.py
+++ Social_network/chat_app/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from .forms import AddChatMemberForm, CreateGroupChatForm, GroupChatUpdateForm
 
 
 # Create your views here.
 class ChatView(LoginRequiredMixin, TemplateView):
     template_name = 'chat_app/chats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_member_form'] = AddChatMemberForm()
+        context['create_group_form'] = CreateGroupChatForm()
+        context['group_chat_update_form'] = GroupChatUpdateForm()
+
+        return context
